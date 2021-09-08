99ffac629f427493e08eda2ba6601ce7783c69f1
Matthias Klebe <matthias.klebe@gmail.com>
2021-09-08 19:58:08 +0200

    Possibly fixes heroku errors

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -12,9 +12,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.API_PORT || 3000;
+  const port = process.env.PORT || 3000;
   await app.listen(port, () => {
-    Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
+    console.log('Listening at http://localhost:' + port + '/' + globalPrefix);
   });
 }
 
