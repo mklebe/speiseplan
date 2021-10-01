a6fbb42652b3192fead389577492581f06e4db65
Matthias Klebe <matthias.klebe@gmail.com>
2021-10-01 15:17:02 +0200

    Adds model project to build

---
+++ libs/models/src/migrations/20211001131453_adds_recipe_and_ingredients/migration.sql
@@ -0,0 +1,29 @@
+-- CreateTable
+CREATE TABLE "Ingredient" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Ingredient_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Recipe" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Recipe_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "IngredientsInRecipe" (
+    "recipeId" INTEGER NOT NULL,
+    "ingredientId" INTEGER NOT NULL,
+
+    CONSTRAINT "IngredientsInRecipe_pkey" PRIMARY KEY ("recipeId","ingredientId")
+);
+
+-- AddForeignKey
+ALTER TABLE "IngredientsInRecipe" ADD CONSTRAINT "IngredientsInRecipe_recipeId_fkey" FOREIGN KEY ("recipeId") REFERENCES "Recipe"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "IngredientsInRecipe" ADD CONSTRAINT "IngredientsInRecipe_ingredientId_fkey" FOREIGN KEY ("ingredientId") REFERENCES "Ingredient"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
